1e0b19db4ff0189984f66f6853c38f833b5f02ce
Brian Hvarregaard <brian@median.dk>
2022-05-25 11:21:30 +0200

    Enabled Swagger in Production Build

--- PSU_PaymentGateway/Startup.cs
+++ PSU_PaymentGateway/Startup.cs
@@ -44,10 +44,11 @@ namespace PSU_PaymentGateway
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PSU_PaymentGateway v1"));
+                app.UseDeveloperExceptionPage();               
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PSU_PaymentGateway v1"));
 
             app.UseHttpsRedirection();
 
